4f0ad9819deb46085a3f3744899390799e7aa143
Shivansh Raghav <shivanshraghav7@gmai.com>
2023-06-12 23:18:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,7 +6,9 @@ const person = {
 };
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
-
+let team=players
+let team1= players.slice()
+let cap1= JSON.parse(JSON.stringify(person))
 window.players = players;
 window.person = person;
 window.team = team;
